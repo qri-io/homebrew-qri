aaca925325f5cb5e80a2522d11f4b0515babcfd8
Dustin Long <me@dustmop.io>
2019-07-01 15:58:53 -0400

    Publish homebrew formula for release 0.8.2

--- qri.rb
+++ qri.rb
@@ -2,9 +2,9 @@
 class Qri < Formula
   desc "Global dataset version control system built on the distributed web"
   homepage "https://qri.io/"
-  url "https://github.com/qri-io/qri/releases/download/v0.7.3/qri_darwin_386.zip"
-  version "0.7.3"
-  sha256 "195480ebad83402d73a90a1c0b17d529298594d108da5ec336abb8f1a77b88ce"
+  url "https://github.com/qri-io/qri/releases/download/v0.8.2/qri_darwin_amd64.zip"
+  version "0.8.2"
+  sha256 "95e451101feede23eebeeaf27056715ed25e670993420c916db31a9e8f85a346"
 
   def install
     bin.install "qri"
